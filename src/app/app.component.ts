10fde18d7712f1b3c08e37800a8531290ea453bd
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2024-08-01 23:07:12 +0200

    feat(reprojection): final state of reprojection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,33 @@
-import { Component, viewChild } from '@angular/core';
+import { Component, inject, signal, viewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DialogComponent } from "./components/dialog/dialog.component";
+import { IconButtonComponent } from "./components/icon-button/icon-button.component";
+import { DialogAuthActionsComponent } from "./components/dialog-auth-actions/dialog-auth-actions.component";
+import { ApiService } from './services/api.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DialogComponent],
+  imports: [RouterOutlet, DialogComponent, IconButtonComponent, DialogAuthActionsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'ng-content-reproj';
   dialog = viewChild.required(DialogComponent);
+  confirming = signal<null | boolean>(null);
+  apiService = inject(ApiService);
+
+  async confirm() {
+    this.confirming.set(true);
+    await this.apiService.getInfo();
+    this.confirming.set(false);
+  }
+
+  async onAuthAction($event: boolean) {
+    if ($event) {
+      await this.confirm();
+    }
+    this.dialog().close();
+  }
 }
